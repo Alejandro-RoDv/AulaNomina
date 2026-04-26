d049aab806c90e45939f3c1bce878815a9dd0cfc
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-04-26 17:48:03 +0200

    Añade modelo Company

---
+++ backend/app/models/company.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Integer, String
+
+from app.db import Base
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    cif = Column(String, unique=True, index=True, nullable=False)
+    address = Column(String, nullable=True)
+    city = Column(String, nullable=True)
+    province = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
