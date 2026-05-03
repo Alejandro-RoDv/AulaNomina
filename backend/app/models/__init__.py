a73442cdec062523b9598088423d00cdacba614a
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-03 12:56:34 +0200

    Register Incident model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from app.models.user import User
 from app.models.employee import Employee
 from app.models.contract import Contract
 from app.models.company import Company
+from app.models.incident import Incident
