e778bc80161c273b1adb122904c6084d03cfe812
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-09 20:08:35 +0200

    Register work center model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.user import User
 from app.models.employee import Employee
 from app.models.contract import Contract
 from app.models.company import Company
+from app.models.work_center import WorkCenter
 from app.models.incident import Incident
 from app.models.payroll import Payroll
