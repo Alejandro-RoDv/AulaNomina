646017a113e10d232a7c547b968f19efff9f6f60
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-10 17:28:40 +0200

    Register document model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,3 +5,4 @@ from app.models.company import Company
 from app.models.work_center import WorkCenter
 from app.models.incident import Incident
 from app.models.payroll import Payroll
+from app.models.document import Document
