489b39b127623dea454bc8cf89624cf62dc32033
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-10 17:44:56 +0200

    Use Split 12 app entry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import AppSplit12 from './AppSplit12.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppSplit12 />
   </StrictMode>,
 )
