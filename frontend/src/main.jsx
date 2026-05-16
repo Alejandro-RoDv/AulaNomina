4993126c958c4c5bc9df91056390dc87d0119fd6
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-16 21:11:35 +0200

    Mount reports route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import AppSplit12 from "./AppSplit12.jsx";
 import CaseStudiesRoute from "./components/case-studies/CaseStudiesRoute.jsx";
+import ReportsRoute from "./components/reports/ReportsRoute.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppSplit12 />
     <CaseStudiesRoute />
+    <ReportsRoute />
   </StrictMode>
 );
