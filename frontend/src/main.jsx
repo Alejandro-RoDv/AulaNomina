9ae15bf4021c888926be36cdc6186154620e4ece
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-16 12:05:44 +0200

    Mount case studies route

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppSplit12 from './AppSplit12.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppSplit12 from "./AppSplit12.jsx";
+import CaseStudiesRoute from "./components/case-studies/CaseStudiesRoute.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppSplit12 />
-  </StrictMode>,
-)
+    <CaseStudiesRoute />
+  </StrictMode>
+);
