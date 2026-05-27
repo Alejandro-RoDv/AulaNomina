9a99531f6ff18469de5d3415655cccfead6f0a62
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-05-27 20:49:53 +0200

    Añadir catalogo de grupos de cotizacion

---
+++ backend/app/catalogs/contribution_groups.py
@@ -0,0 +1,13 @@
+CONTRIBUTION_GROUPS = [
+    {"code": "1", "description": "Ingenieros y Licenciados"},
+    {"code": "2", "description": "Ingenieros Técnicos, Peritos y Ayudantes Titulados"},
+    {"code": "3", "description": "Jefes Administrativos y de Taller"},
+    {"code": "4", "description": "Ayudantes no Titulados"},
+    {"code": "5", "description": "Oficiales Administrativos"},
+    {"code": "6", "description": "Subalternos"},
+    {"code": "7", "description": "Auxiliares Administrativos"},
+    {"code": "8", "description": "Oficiales de primera y segunda"},
+    {"code": "9", "description": "Oficiales de tercera y Especialistas"},
+    {"code": "10", "description": "Peones"},
+    {"code": "11", "description": "Trabajadores menores de 18 años"},
+]
