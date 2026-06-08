e2c531c6634c25a33ed0cfbcddb8b62e96c1521a
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-06-08 21:51:58 +0200

    Remove legacy contract print styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./payroll-print.css";
-import "./contract-print.css";
-import "./contract-print-clause-fixes.js";
 import "./convenios-ui-fixes.js";
 import App from "./App.jsx";
 
