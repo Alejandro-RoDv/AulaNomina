34427ec41d64f5bd9f4daa84001479513978e1ca
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-06-13 13:20:07 +0200

    Remove technical agreement widgets from user interface

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,6 @@ import "./index.css";
 import "./payroll-print.css";
 import "./contract-print-v5-fixes.js";
 import "./convenios-ui-fixes.js";
-import "./agreement-header-widget.js";
-import "./agreement-parameterization-widget.js";
-import "./agreement-parameterization-editor.js";
-import "./agreement-parameterization-forms.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
