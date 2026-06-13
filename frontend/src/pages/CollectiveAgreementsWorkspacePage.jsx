3b6a0a2f6b0fde39745d0fd05672c8a0e4069ab8
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-06-13 14:21:02 +0200

    Create agreement workspace shell

---
+++ frontend/src/pages/CollectiveAgreementsWorkspacePage.jsx
@@ -0,0 +1,5 @@
+import CollectiveAgreementsManagementPage from "./CollectiveAgreementsPage.jsx";
+
+export default function CollectiveAgreementsWorkspacePage(props) {
+  return <CollectiveAgreementsManagementPage {...props} />;
+}
