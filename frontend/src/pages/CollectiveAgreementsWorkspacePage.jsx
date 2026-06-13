27029683c18545ffb6e258e49d50d63f6b69c4b3
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-06-13 21:45:57 +0200

    Add retroactive salary regularization to agreement workspace

--- frontend/src/pages/CollectiveAgreementsWorkspacePage.jsx
+++ frontend/src/pages/CollectiveAgreementsWorkspacePage.jsx
@@ -4,6 +4,7 @@ import AgreementCriteriaPanel from "../components/agreements/AgreementCriteriaPa
 import AgreementExtraPayPanelV2 from "../components/agreements/AgreementExtraPayPanelV2";
 import AgreementSalaryStructurePanel from "../components/agreements/AgreementSalaryStructurePanel";
 import ContractExtraPaySimulationPanel from "../components/agreements/ContractExtraPaySimulationPanel";
+import SalaryRegularizationPanel from "../components/agreements/SalaryRegularizationPanel";
 import SalaryTableActivationPanel from "../components/agreements/SalaryTableActivationPanel";
 import SalaryTableRevisionPanel from "../components/agreements/SalaryTableRevisionPanel";
 import { fetchCollectiveAgreement } from "../services/collectiveAgreementApi";
@@ -129,6 +130,7 @@ export default function CollectiveAgreementsWorkspacePage(props) {
                 <>
                   <SalaryTableRevisionPanel agreement={agreement} onCompleted={() => loadSelectedAgreement(false)} />
                   <SalaryTableActivationPanel agreement={agreement} onChanged={() => loadSelectedAgreement(false)} />
+                  <SalaryRegularizationPanel agreement={agreement} onGenerated={() => loadSelectedAgreement(false)} />
                   <AgreementExtraPayPanelV2 agreement={agreement} onChanged={() => loadSelectedAgreement(false)} />
                   <ContractExtraPaySimulationPanel agreement={agreement} />
                   <AgreementSalaryStructurePanel agreement={agreement} />
