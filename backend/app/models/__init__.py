44c5beecd6a7d316a977a0a2abda9a8e75f14dda
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-06-27 22:09:32 +0200

    feat: register wage garnishment model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -6,6 +6,7 @@ from app.models.company import Company
 from app.models.work_center import WorkCenter
 from app.models.work_calendar import WorkCalendar
 from app.models.incident import Incident
+from app.models.wage_garnishment import WageGarnishment
 from app.models.payroll import Payroll
 from app.models.payroll_salary_structure import ContractPayrollConcept, PayrollConcept, PayrollItem
 from app.models.document import Document
