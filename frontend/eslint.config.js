e8832b9e79b619e1e29a667def517a12f7f1e904
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-06-28 19:33:53 +0200

    chore(lint): ignore superseded incident table implementation

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'src/components/incidents/IncidentTable.jsx',
+  ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
