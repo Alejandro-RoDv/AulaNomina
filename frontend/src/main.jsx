ea5b39fc34d34893160bedf93d99acbd0e3f37e3
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-07-14 23:17:41 +0200

    Split 35: elimina el montaje global legado de SILTRA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,12 +6,9 @@ import "./payroll-print.css";
 import "./contract-print-v5-fixes.js";
 import "./convenios-ui-fixes.js";
 import App from "./App.jsx";
-import SiltraGlobalLauncher from "./components/siltra/SiltraGlobalLauncher.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {/* Acceso global a la aplicación de escritorio SILTRA simulada. */}
-    <SiltraGlobalLauncher />
   </StrictMode>
 );
