c44203878f1c93a1f6e9706403d801a943576822
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-07-14 20:44:09 +0200

    Split 35: monta el acceso global SILTRA en la cabecera

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,11 @@ import "./payroll-print.css";
 import "./contract-print-v5-fixes.js";
 import "./convenios-ui-fixes.js";
 import App from "./App.jsx";
+import SiltraGlobalLauncher from "./components/siltra/SiltraGlobalLauncher.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <SiltraGlobalLauncher />
   </StrictMode>
 );
