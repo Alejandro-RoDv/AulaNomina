51fc7c257cdbb398d596d23fe94abaf568c10fdb
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-07-16 15:12:34 +0200

    Split 36: mount global affiliation remittance launcher

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,11 @@ import "./payroll-print.css";
 import "./contract-print-v5-fixes.js";
 import "./convenios-ui-fixes.js";
 import App from "./App.jsx";
+import AffiliationRemittanceLauncher from "./components/affiliation/AffiliationRemittanceLauncher.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <AffiliationRemittanceLauncher />
   </StrictMode>
 );
