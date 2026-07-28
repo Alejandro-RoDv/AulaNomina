8d6880d843e66615b88e928b6ed2ae836eb81f08
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-07-28 12:19:02 +0200

    feat(fie): mount SILTRA bridge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import "./convenios-ui-fixes.js";
 import "./affiliation-siltra-bridge.js";
 import App from "./App.jsx";
 import AffiliationSiltraBridge from "./components/siltra/AffiliationSiltraBridge.jsx";
+import FieSiltraBridge from "./components/siltra/FieSiltraBridge.jsx";
 import CraRoute from "./components/cra/CraRoute.jsx";
 import FieRoute from "./components/fie/FieRoute.jsx";
 
@@ -15,6 +16,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <AffiliationSiltraBridge />
+    <FieSiltraBridge />
     <CraRoute />
     <FieRoute />
   </StrictMode>
