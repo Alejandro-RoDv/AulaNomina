e13479347c9fd6b970aedd35639adc16fee793c7
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-08-05 11:26:52 +0200

    feat(cases): montar puente de navegación desde el correo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import "./contract-print-v5-fixes.js";
 import "./convenios-ui-fixes.js";
 import "./affiliation-siltra-bridge.js";
 import App from "./App.jsx";
+import CaseNavigationBridge from "./components/case-studies/CaseNavigationBridge.jsx";
 import AffiliationSiltraBridge from "./components/siltra/AffiliationSiltraBridge.jsx";
 import FieSiltraBridge from "./components/siltra/FieSiltraBridge.jsx";
 import CraRoute from "./components/cra/CraRoute.jsx";
@@ -17,6 +18,7 @@ import MailRoute from "./components/mail/MailRoute.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <CaseNavigationBridge />
     <AffiliationSiltraBridge />
     <FieSiltraBridge />
     <CraRoute />
