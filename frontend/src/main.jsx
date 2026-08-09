de08da061ce2019030e78275882fdd73695b2dc4
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-08-09 11:05:26 +0200

    style(split42): load monthly payroll review styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,7 @@ import "./components/employees/employeeSplit42Refinements.css";
 import "./components/contracts/contractHistoryTableCompact.css";
 import "./components/contracts/contractPrintSplit42.css";
 import "./components/payrolls/payrollSplit42.css";
+import "./components/payrolls/monthlyPayrollReviewSplit42.css";
 import "./components/wage-garnishments/wageGarnishmentSplit42Polish.css";
 import "./components/mail/mailScenarioValidation.css";
 import "./pages/DashboardIntro.css";
