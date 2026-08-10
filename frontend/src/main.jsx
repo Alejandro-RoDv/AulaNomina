a327f195c2e277b7bdafc43ccfeb4c8bbd765dff
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-08-10 10:00:58 +0200

    Split 42: cargar estilos compactos de ficheros AFI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -16,6 +16,7 @@ import "./components/payrolls/payrollPreparationMatrix.css";
 import "./components/payrolls/payrollGenerationStatus.css";
 import "./components/payrolls/salaryConceptsPalette.css";
 import "./components/wage-garnishments/wageGarnishmentSplit42Polish.css";
+import "./components/affiliation/affiliationFilesCompact.css";
 import "./components/mail/mailScenarioValidation.css";
 import "./pages/DashboardIntro.css";
 import "./payroll-print.css";
