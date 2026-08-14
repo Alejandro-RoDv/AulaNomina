2a51a7c9472d43e3dc4ce58b1fb9caeda220c2d1
Alejandro-RoDv <alejandro194ro@gmail.com>
2026-08-14 10:43:50 +0200

    refactor: exponer catálogo formativo enriquecido

--- backend/app/training/__init__.py
+++ backend/app/training/__init__.py
@@ -6,6 +6,12 @@ la Fase B.
 """
 
 from .activity_specs_2026 import ACTIVITY_SPECS_2026, activity_spec
+from .catalog_2026 import (
+    build_training_catalog_2026,
+    get_training_activity_2026,
+    list_training_activities_2026,
+    training_dependency_graph_2026,
+)
 from .course_blueprint_2026 import COURSE_BLUEPRINT_2026, blueprint_summary
 from .official_sources_2026 import OFFICIAL_SOURCES_2026
 
@@ -15,4 +21,8 @@ __all__ = [
     "OFFICIAL_SOURCES_2026",
     "activity_spec",
     "blueprint_summary",
+    "build_training_catalog_2026",
+    "get_training_activity_2026",
+    "list_training_activities_2026",
+    "training_dependency_graph_2026",
 ]
